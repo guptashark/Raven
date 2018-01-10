0b56473fa5b330eafec6a6f53ba9957054650469
Aishwary Gupta <guptashark@gmail.com>
2018-01-09 20:11:45 -0500

    Lay base header foundations for NFA and eNFA

---
+++ Lexa/epsilon_nfa.h
@@ -0,0 +1,17 @@
+#ifndef __EPSILON_NFA_H__
+#define __EPSILON_NFA_H__
+
+/* We need epsilon NFA's
+for ease of conversion from 
+regex to automaton. From an eNFA, 
+we can either simulate it, or construct
+an NFA, or DFA. Either way, we need to
+implement this as a base liason between
+our final automata and the higher level
+regex specifications. */
+
+struct epsilon_nfa_state {
+
+};
+
+#endif
---
+++ Lexa/nfa.h
@@ -0,0 +1,17 @@
+#ifndef __NFA_H__
+#define __NFA_H__
+
+/* It is important to be able to 
+simulate both a DFA and NFA automaton, 
+since in practice, one might be faster 
+than the other, and vice versa. Also, 
+an NFA is a fundamental tool because 
+we can convert regex into some NFA's 
+without epsilon transitions. 
+*/
+
+struct nfa_state {
+
+};
+
+#endif
