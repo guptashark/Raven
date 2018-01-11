4800a215f1cc124eb89594eb324c3f9a7b1d2f5a
Aishwary Gupta <guptashark@gmail.com>
2018-01-10 23:03:14 -0500

    Add barebones Makefile and implement eNFA step fns

--- Lexa/epsilon_nfa.cpp
+++ Lexa/epsilon_nfa.cpp
@@ -10,20 +10,44 @@ class LexaRegex {
 
 class eNFA_state {
 
-	// the char this state holds, ie to get here, 
-	// you need to read this char in the input. 
-	char c;
+	private: 
+		// the char this state holds, ie to get here, 
+		// you need to read this char in the input. 
+		char c;
 
-	// all the epsilon transitions
-	list<eNFA_state *> epsilon_transits;
+		// all the epsilon transitions
+		list<eNFA_state *> epsilon_transits;
 
-	list<eNFA_state *> symbol_transits[256];
+		// all the character transitions
+		list<eNFA_state *> symbol_transits[256];
 
+	public: 
+		// constructor
+		eNFA_state(char c): c(c) {};
+
+		// add a character transition
+
+		
+		int add_transit(char c, eNFA_state *destination) {
+			symbol_transits[c].push_back(destination);
+		};
+
+		int add_e_transit(eNFA_state *destination) {
+			epsilon_transits.push_back(destination);
+		};
+
+		// "step" is to step on the char c in the 
+		// automaton, for simulation. 
+		list<eNFA_state *> *step(char c) {
+			return &(symbol_transits[c]);
+		};
+
+		list<eNFA_state *> *eps_step() {
+			return &epsilon_transits;
+		};
 };
 
 int main(void) {
-
-
 
 	return 0;
 }
