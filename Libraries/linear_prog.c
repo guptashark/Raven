9fdaf4503f7339a0ee9bdc5db69c44d024374835
Aishwary Gupta <guptashark@gmail.com>
2018-01-16 23:54:35 -0500

    Fix bugs relating to affine fn_init

--- Libraries/linear_prog.c
+++ Libraries/linear_prog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // several steps for a linear prog... 
 // the linear prog itself, in any sort of form (all acceptable) 
@@ -18,18 +19,24 @@ struct affine_fn {
 	////////////////////////////
 };
 
-int affine_fn
+int affine_fn_init
 (struct affine_fn **fn_dp, int dimension, float *w, float bias) {
+
+	struct affine_fn *fn_p = malloc(sizeof(struct affine_fn));
+
 
 	return 0;
 
 }
+
+
 
 struct linear_program {
 // We need an affine function 
 // 
 
 };
+
 
 int main(void) {
 
