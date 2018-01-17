7bd12f5cf6eaa0a88e46fc6da9fe19008c380509
Aishwary Gupta <guptashark@gmail.com>
2018-01-16 23:53:04 -0500

    Start a linear prog file with affine fn

---
+++ Libraries/linear_prog.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// several steps for a linear prog... 
+// the linear prog itself, in any sort of form (all acceptable) 
+// the linear prog in standard form, for computing with simplex. 
+
+struct affine_fn {
+
+	// an element of R^n
+	int dimension;
+
+	// the actual vector itself. 
+	float *w;
+
+	// the bias. 
+	float b;
+
+	////////////////////////////
+};
+
+int affine_fn
+(struct affine_fn **fn_dp, int dimension, float *w, float bias) {
+
+	return 0;
+
+}
+
+struct linear_program {
+// We need an affine function 
+// 
+
+};
+
+int main(void) {
+
+	return 0;
+}
